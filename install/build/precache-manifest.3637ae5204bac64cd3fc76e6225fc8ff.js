73c37bafb5cfa2f6fd36f08cca11e66816c887fc
Glory <gremman@sandia.gov>
2019-02-27 19:03:03 -0800

    heroku

---
+++ install/build/precache-manifest.3637ae5204bac64cd3fc76e6225fc8ff.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "f25eb1e19e982c8b45ca3d565de180c1",
+    "url": "/static/media/donut2.f25eb1e1.jpeg"
+  },
+  {
+    "revision": "4da9c7ab09fbc76e33e8",
+    "url": "/static/css/main.4b5c6136.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "838835256c241f9b6b64",
+    "url": "/static/js/2.83883525.chunk.js"
+  },
+  {
+    "revision": "d0df244232a4cf7f7701349204a2b089",
+    "url": "/static/media/donut1.d0df2442.jpg"
+  },
+  {
+    "revision": "581f14d95d6951baea3d4f1f1414d682",
+    "url": "/static/media/donut10.581f14d9.jpg"
+  },
+  {
+    "revision": "ce2bb10fcaf039db6ef4eb6678704bb4",
+    "url": "/static/media/donut11.ce2bb10f.JPG"
+  },
+  {
+    "revision": "ae678ef1df801cf40de63f04541ce8c5",
+    "url": "/static/media/donut12.ae678ef1.jpg"
+  },
+  {
+    "revision": "4da9c7ab09fbc76e33e8",
+    "url": "/static/js/main.4da9c7ab.chunk.js"
+  },
+  {
+    "revision": "7d025df765016652a03a2deb229318af",
+    "url": "/static/media/donut3.7d025df7.jpg"
+  },
+  {
+    "revision": "2997e59729a88a4680e7938357a45bd1",
+    "url": "/static/media/donut4.2997e597.jpg"
+  },
+  {
+    "revision": "488ac719e2f4009863c0e075f568713f",
+    "url": "/static/media/donut5.488ac719.jpg"
+  },
+  {
+    "revision": "89dfcae01ed54ce1274c5c7d84a50c47",
+    "url": "/static/media/donut6.89dfcae0.jpg"
+  },
+  {
+    "revision": "9354401986e6c142c53abc8a05c5dc1c",
+    "url": "/static/media/donut7.93544019.jpg"
+  },
+  {
+    "revision": "28be229d3eaf160a74c6e3130efd1c69",
+    "url": "/static/media/donut8.28be229d.jpg"
+  },
+  {
+    "revision": "63eb841f267031ac6e7e21ac65aa5c4a",
+    "url": "/static/media/donut9.63eb841f.jpg"
+  },
+  {
+    "revision": "b7d49179f5c457ef95848276a96b50fa",
+    "url": "/index.html"
+  }
+];
